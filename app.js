531febf271a9aeb5cc40f67d0cbfad36733d58be
Kathryn Reck <kathrynreck@Kathryns-MacBook-Pro.local>
2020-09-10 15:20:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Entry point file
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
