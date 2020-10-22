6bcea22fd022ba767342d9bb428f114482e5a503
Kathryn Reck <kathrynreck@Kathryns-MBP.lan1>
2020-10-22 13:00:34 -0500

    login and registration functional with email and text verification. Admin page lists users with fucntional delete button and allPets page lists all pets pulled from AAC database.

--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const authController = require('../controllers/auth');
+var session = require('express-session');
+const Handlebars = require('hbs');
+
 
 const router = express.Router();
 
 //home page
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index')
 });
 
 //registration page
@@ -16,5 +21,23 @@ router.get('/register', (req, res) => {
 router.get('/login', (req, res) => {
     res.render('login');
 });
+
+//admin home page
+router.get('/adminHome', authController.listUsers);
+
+//request account page
+router.get('/requestAccount', (req, res) => {
+    res.render('requestAccount');
+});
+
+//verify phone number
+router.get('/phoneVerify', authController.verifyPhone);
+
+//list all pets
+router.get('/allPets', authController.listAllPets);
+
+//logout user
+router.get('/logout', authController.logout);
+
 
 module.exports = router;
